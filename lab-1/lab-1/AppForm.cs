a0fc459b48c18ea11009930f84bbba45c2d8cc52
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-04 23:08:24 +0300

    add AppForm.cs

---
+++ lab-1/lab-1/AppForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab_1
+{
+    // Contains main logic of form
+    public partial class AppForm : Form
+    {
+        private Thread receiverThread;
+
+        private Thread senderThread;
+
+        private bool isReceiverThreadContinueWork;
+
+        private bool isSenderThreadActivated;
+
+        private Sender sender;
+
+        private Receiver receiver;
+
+        private readonly static string THREAD_IS_ALREADY_ACTIVE_MESSAGE 
+            = "Wait... previous sending hasn't completed";
+
+        public AppForm()
+        {
+            InitializeComponent();
+
+            // get serial port connection api
+            SerialPortConnectionApi connectionApi = SerialPortConnectionApi.GetInstance();
+
+            // initialize receiver and sender objects
+            this.receiver = new Receiver(connectionApi);
+            this.sender = new Sender(connectionApi);
+
+            receiverThread = new Thread(ReceiverThreadProcedure);
+            isReceiverThreadContinueWork = true;
+            isSenderThreadActivated = false;
+            receiverThread.Start();
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control receiverTextBox.
+        delegate void AddTextToReceiverTextBoxCallback(string text);
+
+        // This method demonstrates a pattern for making thread-safe
+        // calls on a Windows Forms control. 
+        //
+        // If the calling thread is different from the thread that
+        // created the TextBox control, this method creates a
+        // SetTextCallback and calls itself asynchronously using the
+        // Invoke method.
+        //
+        // If the calling thread is the same as the thread that created
+        // the TextBox control, the Text property is set directly. 
+        private void AddTextToReceiverTextBox(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.receiverTextBox.InvokeRequired)
+            {
+                AddTextToReceiverTextBoxCallback d 
+                    = new AddTextToReceiverTextBoxCallback(AddTextToReceiverTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.receiverTextBox.Text += text;
+            }
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control receiverTextBox.
+        delegate void CleanSenderTextBoxCallback();
+
+        private void CleanSenderTextBox()
+        {
+            if (this.senderTextBox.InvokeRequired)
+            {
+                CleanSenderTextBoxCallback d
+                    = new CleanSenderTextBoxCallback(CleanSenderTextBox);
+                this.Invoke(d, new object[] {});
+            }
+            else
+            {
+                this.senderTextBox.Text = "";
+            }
+        }
+
+        private void ReceiverThreadProcedure()
+        {
+            while (isReceiverThreadContinueWork)
+            {
+                string data = receiver.Receive();
+                AddTextToReceiverTextBox(data);
+            }
+        }
+
+        private void SenderThreadProcedure()
+        {
+            string data = senderTextBox.Text;
+            CleanSenderTextBox();
+            this.sender.Send(data);
+            isSenderThreadActivated = false;
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (isSenderThreadActivated)
+            {
+                MessageBox.Show(THREAD_IS_ALREADY_ACTIVE_MESSAGE);
+            }
+            else
+            {
+                isSenderThreadActivated = true;
+                this.senderThread = new Thread(SenderThreadProcedure);
+                this.senderThread.Start();
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            this.receiverTextBox.Text = "";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void ReceptionSpeedComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Int32 receptionSpeed = Convert.ToInt32(receptionSpeedComboBox.SelectedItem);
+            receiver.SetReceptionSpeed(receptionSpeed);
+        }
+
+        private void BaudRateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Int32 baudRate = Convert.ToInt32(baudRateComboBox.SelectedItem);
+            receiver.SetReceptionSpeed(baudRate);
+        }
+    }
+}
