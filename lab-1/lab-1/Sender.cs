eb05fc6f0c93fcbfa1cfc8016374c97db3c879d3
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-04 23:10:09 +0300

    add Sender.cs

---
+++ lab-1/lab-1/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Sender
+    {
+        private SerialPortConnectionApi connectionApi;
+
+        public Sender(SerialPortConnectionApi connectionApi)
+        {
+            this.connectionApi = connectionApi;
+        }
+
+        public void Send(string data)
+        {
+            this.connectionApi.Write(data);
+        }
+
+        public void SetBaudRate(int baudRate)
+        {
+            this.connectionApi.SetBaudRate(baudRate);
+        }
+    }
+}
